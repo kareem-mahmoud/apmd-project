29857f923fad6806c54365fc35e278d983157731
KareemMahmoud <kareem.mahmoud.abd.elhannan@gmail.com>
2025-09-16 14:38:26 +0300

    v2 updates

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,34 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection, 
+         inject, provideAppInitializer } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 
+import { provideTranslateService, TranslateService } from "@ngx-translate/core";
+import { provideTranslateHttpLoader } from "@ngx-translate/http-loader";
+import { provideHttpClient } from '@angular/common/http';
+
+
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
-    provideRouter(routes), provideClientHydration(withEventReplay())
+    provideRouter(routes),
+    provideClientHydration(withEventReplay()),
+    // provideHttpClient(),
+    // provideTranslateService({
+    //   lang: 'en',
+    //   fallbackLang: 'en',
+    //   loader: provideTranslateHttpLoader({
+    //     prefix: '/i18n/',
+    //     suffix: '.json'
+    //   })
+    // }),
+    // provideAppInitializer(() => {
+    //    const  translate = inject(TranslateService);
+    //    translate.use(translate.getBrowserLang() || "en");
+    //  })
+
   ]
 };
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,49 @@
-import { Component, signal } from '@angular/core';
+
+import { HttpClient } from '@angular/common/http';
+import { Component, computed, inject, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+
+import { TranslateModule, TranslateLoader, TranslateService } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+
+import { NgxSkeletonLoaderModule } from 'ngx-skeleton-loader';
+
+// export function HttpLoaderFactory(http: HttpClient) {
+//   return new TranslateHttpLoader();
+// }
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [
+    RouterOutlet,
+    NgxSkeletonLoaderModule,
+    // TranslateModule,
+  ],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
-export class App {
-  protected readonly title = signal('apmd-project');
+export class App implements OnInit {
+  // protected readonly title = signal('Welcome to the APMD Project');
+  // private translate = inject(TranslateService)
+  isLoading = signal(true);
+  data = signal<string>('');
+
+  constructor() {
+    // this.translate.setDefaultLang('en');
+    // const browserLang = this.translate.getBrowserLang();
+    // this.translate.use(browserLang && ['en', 'de'].includes(browserLang) ? browserLang : 'en');
+  }
+
+
+  ngOnInit(): void {
+
+  }
+
+  // switchLanguage(lang: string) {
+  //   this.translate.use(lang);
+  // }
+
+
+
 }
